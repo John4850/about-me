9521869991bd1fd2ba1ca4b63d084411d971781f
JOHN NELSON <JohnNelson4850@gmail.com>
2019-07-16 17:12:42 -0700

    Guessing Game

---
+++ src/guess.js
@@ -0,0 +1,11 @@
+// Elements
+const gb = document.getElementById('gb');
+
+
+// Event Handlers
+gb.onclick = guessNumbers;
+
+function guessNumbers() {
+    console.log('click');
+    let numberDisplay = numSelect.value;
+}
