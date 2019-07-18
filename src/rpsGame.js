d6f086ac9948b562b3fd703282641cd4714f3a3b
JOHN NELSON <JohnNelson4850@gmail.com>
2019-07-18 12:04:10 -0700

    game works in console.log

--- src/rpsGame.js
+++ src/rpsGame.js
@@ -1,14 +1,25 @@
 import getThrow from './rps.js';
+import { didPlayerWin } from './did-player-win.js';
 
 
 const throwButton = document.getElementById('throw-button');
 throwButton.addEventListener('click', () => {
     const computerChoice = getThrow();
     const userChoice = document.querySelector('input:checked').value;
-//    const winner = whoWon(userChoice, computerChoice); 
+    const gameResult = didPlayerWin(userChoice, computerChoice);
     
-
+    display(gameResult);
     console.log(computerChoice + ' - computer \'s throw');
     console.log(userChoice);
-    
 });
+function display(result) {
+    if(result === true) {
+        console.log('Player Wins');
+    }
+    else if(result === false) {
+        console.log('Player Loses');
+    }
+    else if(result === 'draw') {
+        console.log('DRAW');
+    }
+}
