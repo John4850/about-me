1c3399decec389a3d057977a25961b541150fa82
sanvean74 <dlambertpdx@gmail.com>
2019-07-18 17:36:40 -0700

    ui working

--- src/rpsGame.js
+++ src/rpsGame.js
@@ -3,10 +3,24 @@ import { didPlayerWin } from './did-player-win.js';
 
 
 const throwButton = document.getElementById('throw-button');
+const result = document.getElementById('result');
+const message = document.getElementById('message');
+const winCount = document.getElementById('win-count');
+const lossCount = document.getElementById('loss-count');
+const drawCount = document.getElementById('draw-count');
+
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
 throwButton.addEventListener('click', () => {
     const computerChoice = getThrow();
     const userChoice = document.querySelector('input:checked').value;
     const gameResult = didPlayerWin(userChoice, computerChoice);
+
+    result.classList.remove('hidden');
+    const src = 'assets/' + computerChoice + '.png';
+    result.src = src;
     
     display(gameResult);
     console.log(computerChoice + ' - computer \'s throw');
@@ -14,12 +28,18 @@ throwButton.addEventListener('click', () => {
 });
 function display(result) {
     if(result === true) {
-        console.log('Player Wins');
+        message.textContent = 'Player Wins!!';
+        wins++;
+        winCount.textContent = wins;
     }
     else if(result === false) {
-        console.log('Player Loses');
+        message.textContent = 'Player Loses!!';
+        losses++;
+        lossCount.textContent = losses;
     }
     else if(result === 'draw') {
-        console.log('DRAW');
+        message.textContent = 'Player Draws!!';
+        draws++;
+        drawCount.textContent = draws;
     }
 }
